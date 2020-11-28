ade9e4bbc1ba68e5acf72a6320ce0576b6af9588
Jake Basten <jbasten@jakes-mbp.lan>
2020-11-28 15:57:30 -0600

    api gateway is failing with the current lambda response, trying to fix 2

--- src/handlers/hello-world.js
+++ src/handlers/hello-world.js
@@ -1,8 +1,11 @@
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
-        headers: {},
-        body: { message: 'Hello World from blog-server!' },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        },
+        body: JSON.stringify({ message: 'Hello World from blog-server!' }),
         isBase64Encoded: false
     };
 }
