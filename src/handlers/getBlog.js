e2ae9268645c2bc6bd0e3ff3c3023613475195ab
Jake Basten <jbasten@Jakes-MacBook-Pro.local>
2020-12-01 22:42:40 -0600

    adding in route param for fetching blogs dynamically

--- src/handlers/getBlog.js
+++ src/handlers/getBlog.js
@@ -2,8 +2,9 @@ const AWS = require('aws-sdk');
 const { goodResponse } = require('../utils/responseUtil');
 
 exports.handler = async (event, context) => {
+  const blogId = event.pathParameters.id;
   const s3Client = new AWS.S3();
-  const params = { Bucket: 'jbasten-blog', Key: 'markdowns/test.md' }
+  const params = { Bucket: 'jbasten-blog', Key: `markdowns/${blogId}.md` }
 
   const data = await s3Client.getObject(params, (err) => {
     if (err) {
