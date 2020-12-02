3cfd46c890d825e2334b105c96bed82713e2f877
Jake Basten <jbasten@Jakes-MacBook-Pro.local>
2020-12-01 23:22:21 -0600

    added error handling for when blogs aren't found on s3

--- src/handlers/getBlog.js
+++ src/handlers/getBlog.js
@@ -5,11 +5,24 @@ exports.handler = async (event, context) => {
   const s3Client = new AWS.S3();
   const params = { Bucket: 'jbasten-blog', Key: `markdowns/${blogId}.md` }
 
-  const data = await s3Client.getObject(params, (err) => {
-    if (err) {
-      console.log('Error getting s3 object:', err);
-    }
-  }).promise();
+  let data;
+
+  try {
+    data = await s3Client.getObject(params).promise();
+  } catch (e) {
+    console.log('Error getting s3 object:', e);
+  }
+
+  console.log('DATA', data);
+
+  let responseBody = {};
+  if (data) {
+    responseBody.fileContents = data.Body.toString();
+  } else {
+    responseBody.fileContents = '';
+    responseBody.errorMessage = `Could not fetch blog with id ${blogId}`
+  }
+  
 
   return {
     statusCode: 200,
@@ -17,7 +30,7 @@ exports.handler = async (event, context) => {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true
     },
-    body: JSON.stringify({ fileContents: data.Body.toString() }),
+    body: JSON.stringify(responseBody),
     isBase64Encoded: false
   };
 }
