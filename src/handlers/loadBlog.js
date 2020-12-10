51ae2655a02e3ed46bfc1059383b072b84a96007
Jake Basten <jbasten@Jakes-MacBook-Pro.local>
2020-12-09 22:27:18 -0600

    building s3 copy of loadBlog function

--- src/handlers/loadBlog.js
+++ src/handlers/loadBlog.js
@@ -1,3 +1,45 @@
+const AWS = require('aws-sdk');
+
+const BUCKET = 'jbasten-blog';
+const PREFIX = 'blogs/assets-loader/';
+
 exports.handler = async (event, context) => {
-  console.log('hello world from loadBlog');
+  const id = event.id;
+  const title = event.title;
+
+  const markdownFileName = `${id}.md`;
+  const previewImageFileName = `${id}.png`;
+
+  const s3Client = new AWS.S3();
+  const sourceBucketParams = {
+    Bucket: BUCKET,
+    Delimiter: '/',
+    Prefix: PREFIX
+  };
+
+  const objectsReturnedFromS3BlogLoader = await s3Client.listObjects(sourceBucketParams).promise();
+  const sourceAssets = objectsReturnedFromS3BlogLoader.Contents.filter(item => item.Size > 0);
+  const markdown = sourceAssets.find(item => item.Key.endsWith(markdownFileName));
+  const previewImage = sourceAssets.find(item => item.Key.endsWith(previewImageFileName));
+
+  const destinationPrefix = `blogs/${id}`;
+  const copyMarkdownParams = {
+    Bucket: BUCKET,
+    CopySource: `${BUCKET}/${markdown.Key}`,
+    Key: `${destinationPrefix}/${markdownFileName}`,
+  };
+  const copyPreviewImageParams = {
+    Bucket: BUCKET,
+    CopySource: `${BUCKET}/${previewImage.Key}`,
+    Key: `${destinationPrefix}/${previewImageFileName}`,
+  };
+
+  const copyMarkdownData = await s3Client.copyObject(copyMarkdownParams).promise();
+  const copyPreviewImageData = await s3Client.copyObject(copyPreviewImageParams).promise();
+
+  if (copyMarkdownData.CopyObjectResult && copyPreviewImageData.CopyObjectResult) {
+    return {message: 'Objects Copied'}
+  } else {
+    return {error: 'Copy Failed'}
+  }
 }
