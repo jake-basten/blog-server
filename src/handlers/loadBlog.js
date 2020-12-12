71b238a2a99124fb190cb0959763d9d00357c57a
Jake Basten <jrbas94@gmail.com>
2020-12-12 10:44:07 -0600

    don't think i need AWS SDK as a lambda layer as it should come for free, so am removing it

--- src/handlers/loadBlog.js
+++ src/handlers/loadBlog.js
@@ -37,9 +37,7 @@ exports.handler = async (event, context) => {
 
   const copyMarkdownData = await s3Client.copyObject(copyMarkdownParams).promise();
   const copyPreviewImageData = await s3Client.copyObject(copyPreviewImageParams).promise();
-
   const uri = process.env.BLOG_MONGO_CONNECTION;
-  console.log('uri', uri);
   const client = new MongoClient(uri, { useNewUrlParser: true });
   client.connect(err => {
     const collection = client.db("Blog").collection("Blogs");
