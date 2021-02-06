d10c4325408f08b3f612dbae66f2e72b86b95baa
Jake Basten <jrbas94@gmail.com>
2021-02-05 23:50:11 -0600

    adding image path to the response

--- src/handlers/getBlog.js
+++ src/handlers/getBlog.js
@@ -1,21 +1,27 @@
 const AWS = require('aws-sdk');
 
+const BUCKET = 'jbasten-blog';
+
 exports.handler = async (event, context) => {
   const blogId = event.pathParameters.id;
   const s3Client = new AWS.S3();
-  const params = { Bucket: 'jbasten-blog', Key: `blogs/${blogId}/${blogId}.md` }
+  const markdownParams = { Bucket: BUCKET, Key: `blogs/${blogId}/${blogId}.md` };
+  const imageParams = { Bucket: BUCKET, Key: `blogs/${blogId}/${blogId}.png` };
 
-  let data;
+  let markdownData;
+  let imageData;
 
   try {
-    data = await s3Client.getObject(params).promise();
+    markdownData = await s3Client.getObject(markdownParams).promise();
+    imageData = await s3Client.getObject(imageParams).promise();
   } catch (e) {
-    console.log('Error getting s3 object:', e);
+    console.log('Error getting blog data from s3:', e);
   }
 
   let responseBody = {};
-  if (data) {
-    responseBody.fileContents = data.Body.toString();
+  if (markdownData && imageData) {
+    responseBody.fileContents = markdownData.Body.toString();
+    responseBody.imagePath = `https://jbasten-blog.s3.amazonaws.com/blogs/${blogId}/${blogId}.png`;
   } else {
     responseBody.fileContents = '';
     responseBody.errorMessage = `Could not fetch blog with id ${blogId}`
